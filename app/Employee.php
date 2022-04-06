ad758e04123ce1622c6616fc25d197672447acd9
manixcoder <pathakmanish86@gmail.com>
2022-04-06 23:07:13 +0530

    Update seeders 3

--- app/Employee.php
+++ app/Employee.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Employee extends Model
 {
-	
+
 	protected $primaryKey = 'id';
 	protected $table = 'employees';
 	public $timestamps = true;
--- app/Service_sector.php
+++ app/Service_sector.php
@@ -7,12 +7,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Service_sector extends Authenticatable {
+class Service_sector extends Authenticatable
+{
 
     use SoftDeletes;
 
     protected $fillable = [
         'sector', 'status'
     ];
-
 }
